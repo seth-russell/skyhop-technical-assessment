00c368216712b1b547bb1d595be66c9117c312cc
Seth Russell <sarah.russell12@ncf.edu>
2024-03-18 16:01:44 -0400

    Improved Dropdown Component
    - Component now responsive
    - Refactored code for readability

--- src/app/Components/Dropdown.js
+++ src/app/Components/Dropdown.js
@@ -2,26 +2,17 @@ import styles from '@/app/ui/Dropdown.module.css';
 import ToleranceIcon from './ToleranceIcon';
 
 export default function Dropdown(props) {
-	function options() {
-		let opts = [];
-		if (props.title) {
-			opts.push(<option value="" key="title" className={styles.option}>{props.title}</option>);
-		}
-		for (const option of props.options) {
-			opts.push(<option value={option} key={option} className={styles.option}>{option}</option>);
-		}
-		return opts;
-	}
-
-	const selectClassNames = `${styles.select} ${props.label ? styles.labeled : styles.titled}`;
 
 	return (
 		<div className={styles.main}>
 			{props.label && (<label className={styles.label} for={props.id}>{props.label}</label>)}
-			<select name={props.name} id={props.id} className={selectClassNames}>
-				{options()}
-			</select>
-			{props.toleranceToggled && (<ToleranceIcon />)}
+			<div className={props.label ? styles.partialwidth : styles.fullwidth}>
+				<select name={props.name} id={props.id} className={styles.select}>
+					{props.title && <option value="" key="title">{props.title}</option>}
+					{props.options.map((opt) => <option value={opt} key={opt}>{opt}</option>)}
+				</select>
+				{props.toleranceToggled && (<ToleranceIcon />)}
+			</div>
 		</div>
 	);
 }
