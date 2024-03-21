6270c08dc7e701a433875ce3a00f7b4c030f634d
Seth Russell <sarah.russell12@ncf.edu>
2024-03-21 15:45:51 -0400

    Better FileUpload
    - Improved the functionality of FileUpload using useRef
    - Button in FileUpload also opens the file upload input
    - Adjusted css in Dropdown component to fix icon alignment

--- src/app/Components/FileUpload.js
+++ src/app/Components/FileUpload.js
@@ -1,18 +1,26 @@
 import styles from '@/app/ui/FileUpload.module.css';
 import { RiFileReduceFill } from "react-icons/ri";
+import { useRef } from "react";
 
 export default function FileUpload(props) {
+	const inputRef = useRef(null);
+
+	const handleClick = () => {
+		inputRef.current.click();
+	}
+
 	const onChange = (event) => {
 		props.handleFileChange(event.target.files[0]);
 	}
 
 	return (
 		<div className={styles.main}>
+			<input type="file" accept=".csv" onChange={onChange} className={styles.hiddeninput} ref={inputRef}  />
 			<div className={styles.inner}>
 				<RiFileReduceFill className={styles.icon} />
-				<p className={styles.browse}>Drag & Drop Here Or <input type="file" accept=".csv" onChange={onChange} /></p>
+				<span className={styles.browse} onClick={handleClick}>Drag & Drop Here Or <b>Browse</b></span>
 			</div>
-			<button className={styles.button}>Upload Manifest</button>
+			<button className={styles.button} onClick={handleClick}>Upload Manifest</button>
 		</div>
 	)
 }
